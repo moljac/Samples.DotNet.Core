560f8f0e5ef14ee0810c838255aa26b5d7213af7
moljac Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-12-10 10:58:50 +0100

    nunit

---
+++ JetBrains.IntelliJ.Rider/DotNet/DotNet.TestsUnit.NUnit/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DotNet.TestsUnit.NUnit
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
