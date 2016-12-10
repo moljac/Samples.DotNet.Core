0d4517e5bf0957052ca112cdc214b2b0bda090cc
moljac Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-12-10 11:28:46 +0100

    xunit

---
+++ JetBrains.IntelliJ.Rider/DotNet.Core/DotNet.Core.TestsUnit.xUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("DotNet.Core.TestsUnit.xUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNet.Core.TestsUnit.xUnit")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("05B754F1-C339-4300-8796-12A2D20E2565")]
---
+++ JetBrains.IntelliJ.Rider/DotNet.Core/DotNet.Core.TestsUnit.xUnit/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace DotNet.Core.TestsUnit.xUnit
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
