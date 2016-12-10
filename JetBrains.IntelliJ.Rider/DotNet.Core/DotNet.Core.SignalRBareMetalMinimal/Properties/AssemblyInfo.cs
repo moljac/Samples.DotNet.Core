72041ef0a1dfbae519864d853e4413dcec187971
moljac Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-12-10 11:03:22 +0100

    SignalR bare metal

---
+++ JetBrains.IntelliJ.Rider/DotNet.Core/DotNet.Core.SignalRBareMetalMinimal/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("DotNet.Core.SignalRBareMetalMinimal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNet.Core.SignalRBareMetalMinimal")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("1835E6CD-7285-4D86-AA2A-9BD889009940")]
---
+++ JetBrains.IntelliJ.Rider/DotNet.Core/DotNet.Core.SignalRBareMetalMinimal/SignalR/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿
+namespace DotNet.Core.SignalRBareMetalMinimal.SignalR
+{
+    public partial class ChatHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        public static System.Collections.Generic.List<string> ConnectedUsers;
+
+        public void Send(string originatorUser, string message)
+        {
+            Clients.All.messageReceived(originatorUser, message);
+        }
+
+        public void Connect(string newUser)
+        {
+            if (ConnectedUsers == null)
+                ConnectedUsers = new System.Collections.Generic.List<string>();
+
+            ConnectedUsers.Add(newUser);
+            Clients.Caller.getConnectedUsers(ConnectedUsers);
+            Clients.Others.newUserAdded(newUser);
+        }
+    }
+}
