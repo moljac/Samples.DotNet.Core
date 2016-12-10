1c8c8e269531d7d1ec35b407a70afba8153ea125
moljac Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-12-10 11:05:34 +0100

    nunit

---
+++ JetBrains.IntelliJ.Rider/DotNet.Core/DotNet.Core.TestsUnit.NUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("DotNet.Core.TestsUnit.NUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNet.Core.TestsUnit.NUnit")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("17DDC9CA-D4EC-47E7-8FF3-6FCAC0BC501C")]
