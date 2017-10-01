6ce62d2ddd581da765ae0d690877dccb5a8ef475
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-10-01 16:46:25 +0200

    01-web-2-mvc added files

---
+++ ASP.net.Core/diffs-project-contents/01-web-2-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _01_web_2_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
